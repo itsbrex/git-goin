9510a1a19c51d99b66bcdea2e6e56b4427fbcdb3
itsbrex <roach7@gmail.com>
2023-09-24 12:35:00 -0700

    timing

--- open.js
+++ open.js
@@ -1,8 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const vscodeUrl = urlParams.get('url');
 chrome.runtime.sendMessage({ vscodeUrl: vscodeUrl }, function (response) {
-	console.log(response);
-	setTimeout(function () {
-		window.close();
-	}, 4000); // delay in milliseconds
+  console.log(response);
+  setTimeout(function () {
+    window.close();
+  }, 3000); // delay in milliseconds
 });
